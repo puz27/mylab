79886cbadad371b37494a00072614ba7355ebb7f
puz27 <n.avramenko87@gmail.com>
2020-11-11 12:04:26 +0300

    1

--- spisok.py
+++ spisok.py
@@ -7,6 +7,7 @@ spisok=['port',
         'ip']
 print(spisok)
 
+
 print(spisok[2])
 
 spisok.append(100)
@@ -17,3 +18,27 @@ print(spisok)
 list5=[1,2,3]
 list5.extend([200,366,'my'])
 print(list5)
+
+#delete dannyih
+list5=[2,5,3,44,3,111]
+list5.remove(2)
+list5.sort()
+print(list5)
+
+print('Zamena interface')
+my_list='interface gigabit0/0'
+my_list=my_list.replace("gigabit","ethernet")
+print(my_list)
+
+my_list='ASDF:1DFF:DFVC'
+my_list=my_list.replace(':','.')
+print(my_list)
+
+#konvert to dvoicnaya
+print('10 -> 2')
+ip2="{:b}.{:b}.{:b}.{:b}"
+ip10="{:d}.{:d}.{:d}.{:d}"
+ip2=ip2.format(192,168,100,1)
+ip10=ip10.format(192,168,100,1)
+print(ip10)
+print(ip2)
