378f3f9ab3f8ba0a88a2092d2650f4c514b9dee8
puz27 <n.avramenko87@gmail.com>
2020-11-11 10:43:41 +0300

    with format

--- working.py
+++ working.py
@@ -1,3 +1,22 @@
 stroka="data{}"
 stroka=stroka.format(1)
 print(stroka)
+
+stroka2="{:5} {:6}"
+a='12345'
+b='1234567888888888'
+
+stroka2=stroka2.format(a,b)
+print(stroka2)
+
+
+
+shablon='''
+    interface = {}
+    vlan = {}
+    '''
+
+rezult=shablon.format('g0/1',2)
+print(rezult)
+
+
