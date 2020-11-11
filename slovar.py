d0ba9e43047a6e154dd54caa7b3f5c0bc1b82c1c
puz27 <n.avramenko87@gmail.com>
2020-11-11 14:00:15 +0300

    1

--- slovar.py
+++ slovar.py
@@ -3,3 +3,16 @@ slov = { 'host':'cisco',
 
 print(slov['ip'])
 print(slov['host'])
+
+slov2=slov.get('ip')
+print(slov2)
+
+
+
+
+
+print('update dictionary')
+slov1_new ={ 'ip': '1.1.1.1',
+            'version':'new version'}
+slov.update(slov1_new)
+print(slov)
