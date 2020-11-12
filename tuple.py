547e727e508cb05e7e0dbbb7366bed91febc2c00
puz27 <n.avramenko87@gmail.com>
2020-11-12 11:06:31 +0300

    1

--- tuple.py
+++ tuple.py
@@ -3,3 +3,17 @@ print(t1)
 
 l=[1,2,3]
 print(tuple(l))
+print(tuple(l)[2])
+
+
+print('unikalnie dannie')
+sss=[1,2,3,5,5,5,4,3,2]
+unik=set(sss)
+print(unik)
+
+print('mnogestvo')
+vlan1 = {2,4,2}
+vlan2 = {1,7,8,2}
+print('peresechenie')
+peresechenie=vlan1.intersection(vlan2)
+print(peresechenie)
