7cc72e766099d81f5dd24e28ee42869bf940afdd
puz27 <n.avramenko87@gmail.com>
2020-11-13 12:35:24 +0300

    2020-11-13

--- w_type_of_ip.py
+++ w_type_of_ip.py
@@ -1,8 +1,28 @@
 ip = input('insert IP')
 a=ip.split('.')
 #print(a[0])
+#proverka
+'''
+b=int(a[0])
+if b > 255:
+    print('NOT CORRECT IP ADDRESS')
+   '''
+id_add = {'1octet': int(a[0]),
+          '2octet': int(a[1]),
+          '3octet': int(a[2]),
+          '4octet': int(a[3]),
+          }
+print('CHECK IP')
+if id_add['1octet'] > 255:
+    print('ERROR IN 1 OCTET')
+elif  id_add['2octet'] > 255:
+    print('ERROR IN 2 OCTET')
+elif id_add['3octet'] > 255:
+    print('ERROR IN 3 OCTET')
+elif id_add['4octet'] > 255:
+    print('ERROR IN 4 OCTET')
 
-
+print('TYPE OF IP:')
 if ip == '255.255.255.255':
     print('BROADCAST')
 elif ip == '0.0.0.0':
@@ -12,4 +32,4 @@ elif a[0] >='1' and a[0] <= '223':
 elif a[0] >= '224' and a[0] <= '239':
     print('MULTICAST')
 else:
-    print(ip)
+   print('UNUSED')
