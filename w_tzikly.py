506f9ee744a47099319af5fff4556defeb19dbe9
puz27 <n.avramenko87@gmail.com>
2020-11-16 11:09:52 +0300

    2020-11-16

--- w_tzikly.py
+++ w_tzikly.py
@@ -1,14 +1,20 @@
 ip=[]
 s_ip='192.168.1.1'
 octet=s_ip.split('.')
-
-print(octet)
-'''
 '''
 for i in [0,1,2,3]:
    #print('ADD:', i)
    mass='{:08b}'.format(int(octet[i]))
    ip.append(mass)
    print(ip)
+'''
+for x in octet:
+   print(x)
+   ip.append('{:08b}'.format(int(x)))
+   print(ip)
 
-
+interfaces=range(5)
+for z in interfaces:
+   print(z)
+   print("interface GI/{z}")
+   print("interface GI/{}".format(int(z)))
