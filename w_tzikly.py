11ce593a8e1c66e17a29f52d84512c5249d083ca
puz27 <n.avramenko87@gmail.com>
2020-11-16 11:40:46 +0300

    2020-11-16

--- w_tzikly.py
+++ w_tzikly.py
@@ -1,6 +1,7 @@
 ip=[]
 s_ip='192.168.1.1'
 octet=s_ip.split('.')
+
 '''
 for i in [0,1,2,3]:
    #print('ADD:', i)
@@ -8,6 +9,7 @@ for i in [0,1,2,3]:
    ip.append(mass)
    print(ip)
 '''
+"""
 for x in octet:
    print(x)
    ip.append('{:08b}'.format(int(x)))
@@ -18,3 +20,19 @@ for z in interfaces:
    print(z)
    print("interface GI/{z}")
    print("interface GI/{}".format(int(z)))
+"""
+
+data = {'1':10,'2':20}
+switch=['vlan','speed']
+for x in data:
+   print(x)
+   for y in switch:
+      if 'vlan' in y:
+         print(y,data[x])
+      else:
+         print(y)
+
+
+
+
+
