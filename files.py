9d8086a54931b6b24397de360a871a63f27dc56b
puz27 <n.avramenko87@gmail.com>
2020-11-17 11:12:32 +0300

    2020-11-17

--- files.py
+++ files.py
@@ -1,8 +1,15 @@
-with open("file.txt",'r') as x:
-    print(x.read())
+#with open("file.txt",'r') as x:
+   # print(x.read())
 
 
-file=open('file.txt','r')
+#file=open('file.txt','r')
 #print(file.read())
-s=file.readline()
-print(s)
+#s=file.readline()
+#print(file)
+#file.close()
+
+file2=open('file.txt','a+')
+file2.write('linefdgdg235465656546546\n')
+file2.close()
+with open("file.txt",'r') as x:
+  print(x.read())
