1f85bd8c899c364087db56e0960c4e43dfe85459
puz27 <n.avramenko87@gmail.com>
2020-11-18 12:53:10 +0300

    2020-11-18

--- slovar.py
+++ slovar.py
@@ -1,3 +1,4 @@
+'''
 slov = { 'host':'cisco',
          'ip':'150.31.44.1'}
 
@@ -7,12 +8,21 @@ print(slov['host'])
 slov2=slov.get('ip')
 print(slov2)
 
-
-
-
-
 print('update dictionary')
 slov1_new ={ 'ip': '1.1.1.1',
             'version':'new version'}
 slov.update(slov1_new)
 print(slov)
+
+'''
+slovar = {'moscow':{'street':'10','year':'2020'},
+          'ryazan':{'street':'20','year':'2022'}
+          }
+#print(slovar)
+
+for town in slovar:
+    print(town)
+    for info in slovar[town]:
+        print(info)
+        #print(slovar['moscow'])
+
---
+++ w_show_config_1.py
@@ -0,0 +1,39 @@
+'''
+with open('conf.txt') as f:
+    #print(f.read())
+    for line in f:
+        line=line.split()
+        if line[0] != 'information':
+            print(line)
+'''
+slov={}
+
+'''
+with open('conf.txt') as f:
+    #print(f.read())
+    for line in f:
+        line=line.split()
+        if line[1][0].isdigit():
+            # print(line)
+            int,ip,*rest=line
+            slov[int]=ip
+
+print(slov)
+'''
+
+with open('conf2.txt') as f:
+    #print(f.read())
+    for line in f:
+        #print(line)
+        if 'my' in line:
+            int=line.split()[0]
+            print(int)
+
+        if 'mtu' in line:
+            speed=line.split()[1]
+            print(speed)
+            slov[int]=speed
+print(slov)
+
+
+
