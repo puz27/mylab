a2ca19efa5eb80e5891ab113a5fc1f5f4aa9383b
puz27 <n.avramenko87@gmail.com>
2020-11-23 16:32:24 +0300

    2020-11-23

--- w_function_4.py
+++ w_function_4.py
@@ -16,10 +16,11 @@ def generate_trunk_config(trunk_config,trunk_mode_template):
         print(i)
         for z in trunk_mode_template:
             if z.endswith('vlan'):
+                stroka=str(trunk_config.get(i))
+                stroka=stroka.replace('[','').replace(']','')
+                #print(stroka)
 
-                #print('VLAN')
-                 print(str(trunk_config.get(i)))
-                #z=z+str(trunk_config.get(i))
+                z=z+stroka
             print(z)
 
 
