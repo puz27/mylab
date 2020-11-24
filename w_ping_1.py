5beb58529cd925f21d36b97da6e8921d4f875089
puz27 <n.avramenko87@gmail.com>
2020-11-24 15:17:50 +0300

    2020-11-24

--- w_ping_1.py
+++ w_ping_1.py
@@ -1,9 +1,10 @@
 import subprocess
-
+import ipaddress
 ips=['8.8.8.8','192.168.1.1','150.31.44.3']
+ips2=['192.168.1.1-10']
 ip_yes=[]
 ip_no=[]
-
+'''
 def checkip(ips):
     for ip in ips:
        rez=subprocess.run('ping {} -c 2 -n'.format(ip), shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8')
@@ -20,3 +21,20 @@ def checkip(ips):
 
 
 checkip(ips)
+'''
+for ip in ips2:
+    if '-' in ip:
+       # print('mnogestvo')
+        start_ip,end_ip=ip.split('-')
+        #print(start_ip)
+        #print(start_ip.split('.')[:-1])
+        end = ".".join(start_ip.split('.')[:-1]+[end_ip])
+        #print(end)
+        s=ipaddress.ip_address(start_ip)
+        e=ipaddress.ip_address(end)
+        print(range(int(s),int(e)))
+        for ip in range(int(s),int(e)):
+           #print(ip)
+           print('pinging...',ipaddress.ip_address(ip))
+           subprocess.run('ping {} -c 2 -n'.format(ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                          encoding='utf-8')
