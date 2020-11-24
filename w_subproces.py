4ad10e0b120d2cce05ecaf7da6967c7c80ff8dcb
puz27 <n.avramenko87@gmail.com>
2020-11-24 12:51:08 +0300

    2020-11-24

--- w_subproces.py
+++ w_subproces.py
@@ -1,13 +1,24 @@
 import subprocess
 
-see=subprocess.run("who")
-
-see
-
+#see=subprocess.run("who")
+#see
 #mping=subprocess.run('ping 8.8.8.8 -c 5 -n', shell=True)
 #mping
 
 
-myoing2=subprocess.run('ping 8.8.8.8 -c 5 -n', shell=True, stdout=subprocess.PIPE)
-print(myoing2.stdout)
-print(myoing2.stdout.decode('utf-8'))
+#myping2=subprocess.run('ping 8.8.8.8 -c 5 -n', shell=True, stdout=subprocess.PIPE,encoding='utf-8')
+#print(myping2.stdout)
+#print(myoing2.stdout.decode('utf-8'))
+
+
+def checkip(ip):
+   rez=subprocess.run('ping {} -c5 -n'.format(ip), shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,encoding='utf-8')
+   if rez.returncode == 0:
+      print('yes')
+      return True
+   else:
+      return False
+      print('no')
+
+
+checkip('8.8.8.8')
