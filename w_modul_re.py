e8c2c5e43cd95b74e67011d6995b03e1362d35fb
puz27 <n.avramenko87@gmail.com>
2020-11-25 11:25:03 +0300

    2020-11-25

--- w_modul_re.py
+++ w_modul_re.py
@@ -1,4 +1,9 @@
 import re
-
+'''
 stroka='privet 123'
-re.search('123',stroka)
+print(re.search('123',stroka))
+'''
+# zifri
+log1='fdsfsdf 11:10 dsfsdf'
+info1=re.search('\d\d:\d\d',log1)
+print(info1.group())
