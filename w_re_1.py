8ac4aa5cadec6302e1eb27e114c2154005e9ba2f
puz27 <n.avramenko87@gmail.com>
2020-11-25 16:45:25 +0300

    2020-11-25

--- w_re_1.py
+++ w_re_1.py
@@ -1,14 +1,23 @@
 import re
+data=()
 with open('config_r1') as f:
     #print(f.read())
     for line in f:
-        find=re.search('ip address \S+',line)
+        find=re.search('ip address (?P<ip>\S+) (?P<mask>\S+)',line)
         #print(find.group())
         try:
             if find:
-                print(find.group())
+                 #print(find.group())
+                 ip,mask=find.groups()
+                 #print(ip,mask)
+                 data=data+find.groups()
+                 #print(data)
+                 #print((find.groupdict()))
+
         except ArithmeticError as err:
             pass
+
+    print(data)
 
 
 
