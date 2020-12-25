d05ed2aa6e984267e9ce26ffb49019881bbd027a
puz27 <50394337+puz27@users.noreply.github.com>
2020-12-25 12:34:31 +0300

    Create w_json_1.py

---
+++ w_json_1.py
@@ -0,0 +1,18 @@
+import json
+import requests
+
+with open('json_1.json') as f:
+    file=json.load(f)
+    print(file)
+
+with open('json_2.json','w') as f:
+    json.dump(file,f)
+
+uri = 'https://portal5.cbr.ru/back/rapi2/Messages/?MinDateTime=$date&type=inbox'
+user = ''
+passw = ''
+
+
+resp=requests.get(uri,auth=(user,passw))
+print(resp.status_code)
+print(resp.text)
