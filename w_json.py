bd4f9b958e06b44021440d20cacf52c78c735b86
anv <anv@woori.ru>
2020-12-25 12:02:48 +0300

    1

--- w_json.py
+++ w_json.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 with open('json_1.json') as f:
     file=json.load(f)
@@ -7,5 +8,9 @@ with open('json_1.json') as f:
 with open('json_2.json','w') as f:
     json.dump(file,f)
 
-
-
+uri = 'https://portal5.cbr.ru/back/rapi2/Messages/?MinDateTime=$date&type=inbox'
+user = '89avramenkoNV007750004231'
+passw = 'N767fa32!QAZ1qaz'
+resp=requests.get(uri,auth=(user,passw))
+print(resp.status_code)
+print(resp.text)
